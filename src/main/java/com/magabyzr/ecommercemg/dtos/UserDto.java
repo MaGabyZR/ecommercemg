19f2f31731ac2018c08f4a3ff63ea1e8733ab06d
Gabriela Rubio <gabyrubioa@gmail.com>
2025-07-16 10:49:47 -0600

    Customizing response data with annotations.

--- src/main/java/com/magabyzr/ecommercemg/dtos/UserDto.java
+++ src/main/java/com/magabyzr/ecommercemg/dtos/UserDto.java
@@ -1,13 +1,26 @@
 package com.magabyzr.ecommercemg.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter                                                 //So Spring can get the data from these fields and create Json objects.
 public class UserDto {
+    //@JsonProperty("user_id")
     private Long id;
+    //@JsonIgnore
     private String name;
     private String email;
+/*    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String phoneNumber;*/
 
 }
--- src/main/java/com/magabyzr/ecommercemg/mappers/UserMapper.java
+++ src/main/java/com/magabyzr/ecommercemg/mappers/UserMapper.java
@@ -3,8 +3,10 @@ package com.magabyzr.ecommercemg.mappers;
 import com.magabyzr.ecommercemg.dtos.UserDto;
 import com.magabyzr.ecommercemg.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    //@Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     UserDto toDto(User user);
 }
