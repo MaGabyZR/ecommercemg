9f8608a7d2503e8b2b1241312caa377c6f644ce2
Gabriela Rubio <gabyrubioa@gmail.com>
2025-07-17 11:15:22 -0600

    Accepting a POST request from Postman.

--- src/main/java/com/magabyzr/ecommercemg/controllers/UserController.java
+++ src/main/java/com/magabyzr/ecommercemg/controllers/UserController.java
@@ -43,4 +43,10 @@ public class UserController {
 
         return ResponseEntity.ok(userMapper.toDto(user));
     }
+
+    //to accept POST requests.
+    @PostMapping
+    public UserDto createUser(@RequestBody UserDto data){
+        return data;
+    }
 }
