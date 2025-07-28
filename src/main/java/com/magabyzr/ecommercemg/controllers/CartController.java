effa1c81b5304b35253aa78a00da29c6bed5a1c9
Gabriela Rubio <gabyrubioa@gmail.com>
2025-07-28 12:12:14 -0600

    Creating APIs: adding a Product to the cart and give the correct responses, 404, 400 and 200.

--- src/main/java/com/magabyzr/ecommercemg/controllers/CartController.java
+++ src/main/java/com/magabyzr/ecommercemg/controllers/CartController.java
@@ -1,16 +1,21 @@
 package com.magabyzr.ecommercemg.controllers;
 
+import com.magabyzr.ecommercemg.dtos.AddItemToCartRequest;
 import com.magabyzr.ecommercemg.dtos.CartDto;
+import com.magabyzr.ecommercemg.dtos.CartItemDto;
 import com.magabyzr.ecommercemg.entities.Cart;
+import com.magabyzr.ecommercemg.entities.CartItem;
 import com.magabyzr.ecommercemg.mappers.CartMapper;
 import com.magabyzr.ecommercemg.repositories.CartRepository;
+import com.magabyzr.ecommercemg.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.UUID;
 
 @AllArgsConstructor
 @RestController
@@ -18,6 +23,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class CartController {
     private final CartRepository cartRepository;
     private final CartMapper cartMapper;
+    private final ProductRepository productRepository;
 
     @PostMapping
     public ResponseEntity<CartDto> createCart(
@@ -34,5 +40,43 @@ public class CartController {
 
         //Option1 without uri
         //return new ResponseEntity<>(cartDto, HttpStatus.CREATED);
+    }
+
+    //Add items to a cart
+    @PostMapping(("/{cartId}/items"))
+    public ResponseEntity<CartItemDto> addToCart(
+            @PathVariable UUID cartId,
+            @RequestBody AddItemToCartRequest request){
+        var cart = cartRepository.findById(cartId).orElse(null);
+        if (cart == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        var product = productRepository.findById(request.getProductId()).orElse(null);
+        if (product == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        var cartItem = cart.getCartItems().stream()
+                .filter(item -> item.getProduct().getId().equals(product.getId()))
+                .findFirst()
+                .orElse(null);
+
+        if (cartItem != null) {
+            cartItem.setQuantity(cartItem.getQuantity() + 1);
+        } else {
+            cartItem = new CartItem();
+            cartItem.setProduct(product);
+            cartItem.setQuantity(1);
+            cartItem.setCart(cart);
+            cart.getCartItems().add(cartItem);
+        }
+
+        cartRepository.save(cart);
+
+        var cartItemDto = cartMapper.toDto(cartItem);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(cartItemDto);
+
     }
 }
---
+++ src/main/java/com/magabyzr/ecommercemg/dtos/CartProductDto.java
@@ -0,0 +1,12 @@
+package com.magabyzr.ecommercemg.dtos;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class CartProductDto {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+}
