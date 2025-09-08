4e27c608f49a576388a263a87d94d49004467846
Gabriela Rubio <gabyrubioa@gmail.com>
2025-09-08 13:32:48 -0600

    Checkoutandorder: Creating the webhook endpoint.

--- src/main/java/com/magabyzr/ecommercemg/controllers/CheckoutController.java
+++ src/main/java/com/magabyzr/ecommercemg/controllers/CheckoutController.java
@@ -7,22 +7,56 @@ import com.magabyzr.ecommercemg.exceptions.CartEmptyException;
 import com.magabyzr.ecommercemg.exceptions.CartNotFoundException;
 import com.magabyzr.ecommercemg.exceptions.PaymentException;
 import com.magabyzr.ecommercemg.services.CheckoutService;
+import com.stripe.exception.SignatureVerificationException;
+import com.stripe.net.Webhook;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/checkout")
 public class CheckoutController {
     private final CheckoutService checkoutService;
 
+    @Value("${stripe.webhookSecretKey}")
+    private String webhookSecretKey;
+
     @PostMapping
     public CheckoutResponse checkout(@Valid @RequestBody CheckoutRequest request){
         return checkoutService.checkout(request);
+    }
+    //Webhook endpoint
+    @PostMapping("/webhook")
+    public ResponseEntity<Void> handleWebhook(
+            //verify the request came from Stripe and it was a legit request.
+            @RequestHeader("Stripe-Signature") String signature,
+            @RequestBody String payload
+    ){
+        //Parse the payload.
+        try {
+            var event = Webhook.constructEvent(payload, signature, webhookSecretKey);
+            System.out.println(event.getType());
+
+            var stripeObject = event.getDataObjectDeserializer().getObject().orElse(null);
+
+            switch (event.getType()) {
+                case "payment_intent.succeeded" -> {
+                    //UPDATE order status to (PAID)
+                }
+                case "payment_intent.failed" -> {
+                    //UPDATE order status (FAILED)
+                }
+            }
+            return ResponseEntity.ok().build();
+
+        } catch (SignatureVerificationException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
     //Handle payment exceptions, in case we cannot create a Checkout Session.
     @ExceptionHandler({PaymentException.class})
