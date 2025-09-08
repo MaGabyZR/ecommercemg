05fe848141dcf7e7a4b070b33125d0e09287de50
Gabriela Rubio <gabyrubioa@gmail.com>
2025-09-08 12:12:38 -0600

    Checkoutandorder: Refactoring, decoupling from Stripe.

---
+++ src/main/java/com/magabyzr/ecommercemg/services/StripePaymentGateway.java
@@ -0,0 +1,63 @@
+package com.magabyzr.ecommercemg.services;
+
+import com.magabyzr.ecommercemg.entities.Order;
+import com.magabyzr.ecommercemg.entities.OrderItem;
+import com.magabyzr.ecommercemg.exceptions.PaymentException;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class StripePaymentGateway implements PaymentGateway {
+    @Value("${websiteUrl}")
+    private String websiteUrl;
+
+    @Override
+    public CheckoutSession createCheckoutSession(Order order) {
+        try{
+            //Create a checkout session.
+            var builder = SessionCreateParams.builder()
+                    .setMode(SessionCreateParams.Mode.PAYMENT)
+                    .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
+                    .setCancelUrl(websiteUrl + "/checkout-cancel?orderId=" + order.getId());
+            //add all order items to the builder and for each item create a Stripe line item.
+            order.getItems().forEach(item -> {
+                var lineItem = createLineItem(item);
+                builder.addLineItem(lineItem);
+            });
+            //Build a session and return it.
+            var session = Session.create(builder.build());
+            return new CheckoutSession(session.getUrl());
+        }
+        catch (StripeException ex){
+            System.out.println(ex.getMessage());
+            throw new PaymentException();
+        }
+    }
+
+    private SessionCreateParams.LineItem createLineItem(OrderItem item) {
+        return SessionCreateParams.LineItem.builder()
+                .setQuantity(Long.valueOf(item.getQuantity()))
+                .setPriceData(createPriceData(item))
+                .build();
+    }
+
+    private SessionCreateParams.LineItem.PriceData createPriceData(OrderItem item) {
+        return SessionCreateParams.LineItem.PriceData.builder()
+                .setCurrency("USD")
+                .setUnitAmountDecimal(
+                        item.getUnitPrice().multiply(BigDecimal.valueOf(100)))
+                .setProductData(createProductData(item))
+                .build();
+    }
+
+    private SessionCreateParams.LineItem.PriceData.ProductData createProductData(OrderItem item) {
+        return SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                .setName(item.getProduct().getName())
+                .build();
+    }
+}
